28adfbe95e71049e6217201e7f4d28bbb5a79d57
Dmytro Zl <zl.dmitriy94@gmail.com>
2024-05-12 14:44:26 +0300

    modal fixed

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,94 +8,71 @@ import ErrorMessage from '../ErrorMessage/ErrorMessage';
 import LoadMoreBtn from '../LoadMoreBtn/LoadMoreBtn';
 import ImageModal from '../ImageModal/ImageModal';
 
-
 function App() {
   const [images, setImages] = useState([]);
   const [query, setQuery] = useState("");
-  const [page, setPage] = useState(1); // State to keep track of the current page
-  const [loaderClass, setLoaderClass] = useState("visually-hidden");
-  const [errorMsgClass, setErrorMsgClass] = useState("visually-hidden");
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
   const [selectedImage, setSelectedImage] = useState(null);
-    
-  const fetchImages = useCallback(async (page = 1) => {
-    if (query) {
-      try {
-        setLoaderClass("");
-        const response = await axios.get(
-          `https://api.unsplash.com/search/photos?client_id=WWP0ZkMHW4a1CGgA-GBI0FrCCywjnB3L6d04IFuYIlk&query=${query}&page=${page}`
-        );
-        if (response.data && response.data.results) {
-          const newImages = response.data.results;
-          if (page === 1) {
-            setImages(newImages);
-          } else {
-            setImages(prevImages => {
-              const existingIds = new Set(prevImages.map(img => img.id));
-              const filteredNewImages = newImages.filter(img =>!existingIds.has(img.id));
-              return [...prevImages,...filteredNewImages];
-            });
-          }
-          setLoaderClass("visually-hidden");
-          setErrorMsgClass("visually-hidden");
-        } else {
-          console.error('API response does not contain results data');
-          setImages([]);
-        }
-      } catch (error) {
-        console.error('Error fetching images:', error);
-        setImages([]);
-        setErrorMsgClass("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const fetchImages = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await axios.get(
+        `https://api.unsplash.com/search/photos?client_id=WWP0ZkMHW4a1CGgA-GBI0FrCCywjnB3L6d04IFuYIlk&query=${query}&page=${page}`
+      );
+      const newImages = response.data.results;
+      if (page === 1) {
+        setImages(newImages);
+      } else {
+        setImages(prevImages => [...prevImages,...newImages]);
       }
+    } catch (error) {
+      setError('Failed to fetch images');
+    } finally {
+      setIsLoading(false);
     }
-  }, [query]);
+  }, [query, page]);
 
   useEffect(() => {
+    if (query) {
     fetchImages();
+  }
   }, [fetchImages]);
 
   const loadMore = () => {
-  setPage((prevPage) => {
-    const newPage = prevPage + 1;
-    fetchImages(newPage); 
-    return newPage;
-  });
-};
-  const handleImageClick = (image) => {
+    setPage(prevPage => prevPage + 1);
+  };
+
+  const handleSearchSubmit = (inputValue) => {
+    setQuery(inputValue);
+    setPage(1);
+    setImages([]);
+  };
+const handleImageClick = (image) => {
     setSelectedImage(image);
     setIsModalOpen(true);
   };
-
-  const closeModal = () => {
+   const closeModal = () => {
     setIsModalOpen(false);
   };
-    
-  const resetImages = () => {
-    setImages([]);
-    setPage(1); // Reset the page number as well
-  };
-  
-  // Update this function to handle the new inputValue from SearchBar
-  const handleSearchSubmit = (inputValue) => {
-    setQuery(inputValue);
-    resetImages(); // Reset images and page before fetching new images
-    fetchImages(1); // Fetch images for the new query
-  };
-
   return (
     <div>
       <SearchBar onSubmit={handleSearchSubmit} />
       {images.length > 0 && <ImageGallery images={images} onImageClick={handleImageClick} />}
-      {isModalOpen && selectedImage && (
+       {isModalOpen && selectedImage && (
         <ImageModal
           isOpen={isModalOpen}
           selectedImage={selectedImage}
           onRequestClose={closeModal}
         />
       )}
+      {isLoading && <Loader />}
+      {error && <ErrorMessage message={error} />}
       {images.length > 0 && <LoadMoreBtn loadMore={loadMore} />}
-      <ErrorMessage errorMsgClass={errorMsgClass} />
-      <Loader loaderClass={loaderClass} />
     </div>
   );
 }
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -6,13 +6,14 @@ import styles from "./ImageGallery.module.css";
 export default function ImageGallery({ images, onImageClick }) {
   return (
     <ul className={styles.imageGallery}>
-      {images.map((image, index) => (  // Include the index here
-        <ImageCard
-          key={`${image.id}-${index}`}  // Use the index to ensure unique keys
-          imageUrl={image.urls.small}
-          altText={image.description || 'Image description not available'}
-          onClick={() => onImageClick(image)}
-        />
+      {images.map((image, index) => (
+        <li key={image.id}> 
+          <ImageCard
+            imageUrl={image.urls.small}
+            altText={image.description || 'Image description not available'}
+            onClick={() => onImageClick(image)}
+          />
+        </li>
       ))}
     </ul>
   );
